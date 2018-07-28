2d25976f05d9b9c4f361f191a0fed9a7aa21116f
han.zh <han.zhihong@qq.com>
2018-07-28 23:13:05 +0800

    添加编译信息

--- mosquitto_android_cmake.sh
+++ mosquitto_android_cmake.sh
@@ -8,8 +8,26 @@
 
 # Threading has to be disabled since android doesn't support it fully
 # Also disabled TLS since I couldn't get cmake to find the openssl lib properly
+
+#参数设在这里,支持的NDK版本有r5-r10d
 CURRENT_DIR=$(dirname $(readlink -f $0))
+ANDROID_NDK=${ANDROID_NDK_R9D}
 ANDROID_ABI="armeabi-v7a"
+ANDROID_TOOLCHAIN="arm-linux-androideabi-4.8"
+
+#显示参数
+echo "
+--------------------------------
+--
+--
+mosquitto cmake CURRENT_DIR="${CURRENT_DIR}"
+                ANDROID_NDK="${ANDROID_NDK}"
+                ANDROID_TOOLCHAIN="${ANDROID_TOOLCHAIN}"
+--
+--
+"
+
+
 rm -rf mosquitto/build
 mkdir -p mosquitto/build
 cd mosquitto/build
@@ -18,7 +36,7 @@ cmake \
    -DANDROID_ABI=${ANDROID_ABI} \
    -DANDROID_NDK_HOST_X64="YES" \
    -DANDROID_NATIVE_API_LEVEL=19 \
-   -DANDROID_TOOLCHAIN_NAME="arm-linux-androideabi-4.9" \
+   -DANDROID_TOOLCHAIN_NAME=${ANDROID_TOOLCHAIN} \
    -DCMAKE_TOOLCHAIN_FILE="${CURRENT_DIR}/android-cmake/android.toolchain.cmake" \
    -DOPENSSL_LIBRARIES="${CURRENT_DIR}/openssl/arch-${ANDROID_ABI}/lib" \
    -DOPENSSL_INCLUDE_DIR="${CURRENT_DIR}/openssl/sources/include" \
